2d4e39ef72e9f9964efb8b0931397021f4af992a
Helix088 <pat20001@byui.edu>
2023-01-14 23:10:23 -0700

    created new swagger.json

--- swagger.js
+++ swagger.js
@@ -6,7 +6,7 @@ const doc = {
     description: "Contact API",
   },
   host: "https://chase-week04-ind.onrender.com",
-  schemes: ["http"],
+  schemes: ["https"],
 };
 
 const outputFile = './swagger.json';
