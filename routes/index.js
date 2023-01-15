5ea4f3f18ddab4f871c8e545a4d4bf14a0cea130
Helix088 <pat20001@byui.edu>
2023-01-14 21:54:59 -0700

    fixes

--- routes/index.js
+++ routes/index.js
@@ -3,4 +3,7 @@ const router = express.Router();
 
 router.use("/contacts", require("./contacts"));
 
+router.use("/api-docs", swaggerUi.serve);
+router.get("/api-docs", swaggerUi.setup(swaggerDocument));
+
 module.exports = router;
---
+++ swagger.js
@@ -0,0 +1,15 @@
+const swaggerAutogen = require("swagger-autogen")();
+
+const doc = {
+  info: {
+    title: "My API",
+    description: "Description",
+  },
+  host: "localhost:3000",
+  schemes: ["http"],
+};
+
+const outputFile = "./path/swagger-output.json";
+const endpointsFiles = ["./path/endpointsUser.js", "./path/endpointsBook.js"];
+
+swaggerAutogen(outputFile, endpointsFiles, doc);
