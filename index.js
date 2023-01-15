a396c821cd906e6999fdea93c849ad3ac44532a4
Helix088 <pat20001@byui.edu>
2023-01-14 21:41:08 -0700

    fixes

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const mongodb = require("./db/connect");
 const port = process.env.PORT || 8080;
 const app = express();
 
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json');
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
  
 app
   .use(bodyParser.json())
@@ -15,7 +15,7 @@ app
     next();
   })
   .use("/", require("./routes"))
-  .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  .use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 mongodb.initDb((err, mongodb) => {
   if (err) {
